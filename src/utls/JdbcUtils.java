5f00eb741cb85bacb8e2d31c63bf7f03152cf347
HaohuanJiang <your@email.com>
2023-01-13 21:58:52 +0800

    Initial commit

---
+++ src/utls/JdbcUtils.java
@@ -0,0 +1,2 @@
+package utls;public class JdbcUtils {
+}
---
+++ student_manage_system.sql
@@ -0,0 +1,52 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 8.0.31 : Database - student_manage_system
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`student_manage_system` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `student_manage_system`;
+
+/*Table structure for table `class` */
+
+DROP TABLE IF EXISTS `class`;
+
+CREATE TABLE `class` (
+  `studentid` int NOT NULL COMMENT '学生id',
+  `classid` int NOT NULL COMMENT '班级id、',
+  `time` date NOT NULL COMMENT '入学时间',
+  PRIMARY KEY (`studentid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+/*Data for the table `class` */
+
+insert  into `class`(`studentid`,`classid`,`time`) values (1,1,'2023-01-13'),(2,1,'2023-01-11'),(3,1,'2023-01-08'),(5,2,'2023-01-13');
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `studentid` int NOT NULL COMMENT '学号',
+  `name` varchar(10) NOT NULL COMMENT '姓名',
+  `sex` varchar(2) NOT NULL COMMENT '性别',
+  PRIMARY KEY (`studentid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+/*Data for the table `student` */
+
+insert  into `student`(`studentid`,`name`,`sex`) values (1,'李白','男'),(2,'杜甫','男'),(3,'李清照','女'),(4,'王安石','男'),(5,'李贺','男');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
